b73c5fee6bc82378ba3fd981b01b89d2b6ce2a0b
Alex Craig <acraig780@gmail.com>
2022-03-03 22:52:32 -0500

    added image upload

--- components/rating.js
+++ components/rating.js
@@ -1,8 +1,7 @@
 import React, {useEffect, useState} from 'react';
-import Image from 'next/image';
 import styles from '../styles/rating.module.scss';
 
-export default function Rating({column, rating}) {
+export default function Rating({column, rating, readonly}) {
   const [internalRating, setInternalRating] = useState(rating);
   const [image, setImage] = useState(rating);
 
@@ -21,9 +20,10 @@ export default function Rating({column, rating}) {
   return (
     <div className={styles.column}>
       <span className={styles.name}>{column}</span>
-      {/* <Image src={`/ratings/${image}.png`} width={283} height={324} role='presentation' alt='' /> */}
       <img src={`/ratings/${image}.png`} className={styles.image} role='presentation' alt='' />
-      <input type='number' defaultValue={rating} min={1} max={5} step={0.5} className={styles.rating} onChange={e => setInternalRating(e.target.value)} />
+      {readonly ?
+        <span className={styles.rating}>{rating}</span> :
+        <input type='number' defaultValue={rating} min={1} max={5} step={0.5} className={styles.rating} onChange={e => setInternalRating(e.target.value)} />}
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,29 @@
+import {useEffect, useState} from 'react';
 import Head from 'next/head';
+import clsx from 'clsx';
 import html2canvas from 'html2canvas';
 import Rating from '../components/rating';
 import styles from '../styles/Home.module.scss';
-import { useState } from 'react';
 
 export default function Home() {
   const [gameName, setGameName] = useState('Game Name');
+  const [coverFile, setCoverFile] = useState();
+  const [downloadingImage, setDownloadingImage] = useState(false);
 
-  const downloadImage = () => {
-    html2canvas(document.getElementById('image-root')).then(canvas => {
-      let link = document.createElement('a');
+  useEffect(() => {
+    if (downloadingImage) {
+      html2canvas(document.getElementById('image-root')).then(canvas => {
+        setDownloadingImage(false);
 
-      link.download = `${gameName}-ratings.jpg`;
-      link.target = '_blank';
-      link.href = canvas.toDataURL();
-      link.click();
-    });
-  };
+        let link = document.createElement('a');
+
+        link.download = `${gameName}-ratings.jpg`;
+        link.target = '_blank';
+        link.href = canvas.toDataURL();
+        link.click();
+      });
+    }
+  }, [downloadingImage]);
 
   return (
     <div className={styles.container}>
@@ -33,18 +40,30 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <div id='image-root' className={styles.ratingsContainer}>
-          <input className={styles.gameName} defaultValue={gameName} onChange={e => setGameName(e.target.value)} />
-          <div className={styles.ratingsColumns}>
-            <Rating column={'Gameplay'} rating={1} />
-            <Rating column={'Narrative'} rating={2} />
-            <Rating column={'Graphics'} rating={3} />
-            <Rating column={'X-Factor'} rating={4} />
-            <Rating column={'Overall'} rating={5} />
+        <div id='image-root'>
+          <label htmlFor='cover-upload' className={clsx(styles.coverUpload, {[styles.downloading]: downloadingImage})}>
+            {coverFile ?
+              <img src={URL.createObjectURL(coverFile)} alt={`${gameName} cover art`} /> :
+              <span className={clsx(styles.label, {hidden: downloadingImage})}>Choose a cover image</span>}
+
+            {!downloadingImage ? <input id='cover-upload' type='file' accept="image/*" onChange={e => setCoverFile(e.target.files[0])} /> : null}
+          </label>
+
+          <div className={styles.ratingsContainer}>
+            {downloadingImage ?
+              <h1 className={styles.gameName}>{gameName}</h1> :
+              <input type='text' className={styles.gameName} defaultValue={gameName} onChange={e => setGameName(e.target.value)} />}
+            <div className={styles.ratingsColumns}>
+              <Rating column={'Gameplay'} rating={1} readonly={downloadingImage} />
+              <Rating column={'Narrative'} rating={2} readonly={downloadingImage} />
+              <Rating column={'Graphics'} rating={3} readonly={downloadingImage} />
+              <Rating column={'X-Factor'} rating={4} readonly={downloadingImage} />
+              <Rating column={'Overall'} rating={5} readonly={downloadingImage} />
+            </div>
           </div>
         </div>
 
-        <button type='button' onClick={downloadImage}>Download</button>
+        {!downloadingImage && <button type='button' onClick={() => setDownloadingImage(true)}>Download</button>}
       </main>
 
       <footer className={styles.footer}>
